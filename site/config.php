0afca73a5171148a327103e2ccbb9f95146f57c5
Jakob Jentschke <jakob.jentschke@gmail.com>
2015-03-06 12:35:51 +0100

    test

--- site/config.php
+++ site/config.php
@@ -3,6 +3,6 @@
 $configDB[0]['host'] = 'localhost';
 $configDB[0]['user'] = 'root';
 $configDB[0]['password'] = 'jakob';
-$configDB[0]['database'] = 'db2';
-$configDB['prefix'] = "db2_";
+$configDB[0]['database'] = 'db3';
+$configDB['prefix'] = "db3_";
 ?>
